dd7d3aa7e9fcb002b8f93be574b0eaf5762d42ee
JayLatt <jlattimor3@gmail.com>
2017-08-06 22:28:20 -0400

    push is working and operating

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
+/*
 //Authorization
 const auth = firebase.auth();
 auth.signInWithEmailAndPassword(email, pass); //signs in exiting user
 auth.createUserWithEmailAndPassword(email, pass); //creates a user
 auth.onAuthStateChanged(firebaseUser => { });
+*/
 
 //initial test
 var getTest = document.getElementsByClassName('test')[0];
@@ -10,31 +12,26 @@ var databaseRef = firebase.database().ref().child('text');
 
 databaseRef.on('value', snap => getTest.innerText = snap.val());
 
-//Array of text
-var getMessages = document.getElementsByClassName('roomContents')[0];
-var roomsRef = firebase.database().ref().child('rooms');
-
-databaseRef.on('value', snap => getMessages.innerText = snap.val());
-
 //get the logged in user
 var current_user = '';
 
 //grabs array from firebase
-var messages = firebase.database().ref('messages');
+var messagesRef = firebase.database().ref().child('rooms').child('Room1').child('messages');
 
 //submit button
-var field_content = document.getElementsById('message_field').value;
-var send = documment.getElementsById('send_button');
+var form = document.getElementById('message-form');
 
+var send = document.getElementById('send-button');
 send.addEventListener('click', function(){
   send_message();
 });
 
 //add to message list array
-function send_message(user, content){
-  message_list.$add({
-    user: current_user,
-    content: field_content
+function send_message(){
+  var field_value = document.getElementById('message-field').value;
+  messagesRef.push({
+    user: "dog",
+    content: field_value
   });
-  field_content.reset();
+  form.reset();
 };

f3f9124deb6f62ee92ea8f56b43c72da0364992e
JayLatt <jlattimor3@gmail.com>
2017-08-09 05:31:29 -0400

    got everything done

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,91 @@
-/*
-//Authorization
-const auth = firebase.auth();
-auth.signInWithEmailAndPassword(email, pass); //signs in exiting user
-auth.createUserWithEmailAndPassword(email, pass); //creates a user
-auth.onAuthStateChanged(firebaseUser => { });
-*/
-
 //initial test
 var getTest = document.getElementsByClassName('test')[0];
 var databaseRef = firebase.database().ref().child('text');
 
 databaseRef.on('value', snap => getTest.innerText = snap.val());
 
-//get the logged in user
-var current_user = 'dog';
+//User creation
+function makeAny(){
+  var ran1 = Math.floor((Math.random() * 10000) + 1);
+  var ran2 = Math.floor((Math.random() * 1000) + 1);
+  current_user = "Anonymous_" + ran1 + "!" + ran2;
+  return current_user
+}
+
+function checkUserName(){
+  if(firebase.auth().currentUser.displayName == null){
+    var getUserName = prompt("No username. Please enter a username or leave blank for generated one");
+    if(firebase.auth().currentUser.displayName == null){
+      if (getUserName != '' || getUserName != null) {
+        firebase.auth().currentUser.updateProfile({displayName: getUserName});
+      }else{
+        var tempUser = makeAny();
+        firebase.auth().currentUser.updateProfile({displayName: tempUser});
+      }
+    }
+  }
+}
+
+//Login
+var fieldEmail = document.getElementById('email-field');
+var fieldPassword = document.getElementById('password-field');
+var buttonLogIn = document.getElementById('logIn-button');
+var buttonLogOut = document.getElementById('logOut-button');
+var buttonSignUp = document.getElementById('signUp-button');
+var loginForm = document.getElementById('login-form');
+
+//login
+buttonLogIn.addEventListener('click', e => {
+  const email = fieldEmail.value;
+  const password = fieldPassword.value;
+  const auth = firebase.auth();
+
+  const promise = auth.signInWithEmailAndPassword(email, password);
+  loginForm.reset();
+});
+
+//signup
+buttonSignUp.addEventListener('click', e => {
+  const email = fieldEmail.value;
+  const password = fieldPassword.value;
+  const auth = firebase.auth();
+
+  auth.createUserWithEmailAndPassword(email, password);
+  loginForm.reset();
+});
+
+//logout
+buttonLogOut.addEventListener('click', e => {
+  firebase.auth().signOut();
+  console.log("hoorah!");
+});
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if(firebaseUser != null){
+    checkUserName();
+    console.log(firebaseUser);
+  }
+  else{
+    console.log("nothing");
+  }
+  showLogin();
+});
+
+//shows and hides login elements
+function showLogin(){
+  if(firebase.auth().currentUser){
+    buttonLogIn.style.display = "none";
+    buttonSignUp.style.display = "none";
+    //outlier
+    buttonLogOut.style.display = "inline";
+  }
+  else{
+    buttonLogOut.style.display = "none";
+    //outlier
+    buttonLogIn.style.display = "inline";
+    buttonSignUp.style.display = "inline";
+  }
+}
 
 //grabs array from firebase
 var getElementArray = document.getElementsByClassName('list-msg')[0];
@@ -43,17 +115,43 @@ send.addEventListener('click', function(){
 
 //add to message list array
 function send_message(){
+  if(firebase.auth().currentUser != null){
+    var field_value = document.getElementById('message-field').value;
+    messagesRef.push({
+      user: firebase.auth().currentUser.displayName,
+      content: field_value
+    });
+  }
+  else{
+    alert("Must be logged in to post a message in chat! (leave blank for random generated name)");
+  }
+  form.reset();
+};
+
+/*
+function send_message(){
   var field_value = document.getElementById('message-field').value;
   messagesRef.push({
-    user: current_user,
+    user: firebase.auth().currentUser.displayName,
     content: field_value
   });
   form.reset();
 };
+*/
 
+
+/* old one
+buttonSignUp.addEventListener('click', e => {
+  const email = fieldEmail.value;
+  const password = fieldPassword.value;
+  const auth = firebase.auth();
+
+  const promise = auth.createUserWithEmailAndPassword(email, password);
+  promise.catch(e => console.log(e.message));
+});
+*/
 
 /*
-
 
 // sync down from server
 var getElementArray = document.getElementsByClassName('list-msg')[0];
